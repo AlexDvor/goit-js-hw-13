330e1923020414774cae5e0711880ef69a71fe22
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-07-20 19:25:29 +0200

    added js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './sass/main.scss';
+import API from './js/fetchApi';
+import { debounce } from 'lodash';
+import imgCardTpl from './templates/card.hbs';
+import { func } from 'assert-plus';
+
+
+const refs = {
+    inputValue: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+}
+
+
+
+refs.inputValue.addEventListener('input', debounce(onSearch, 300))
+
+
+function onSearch(e) {
+        const inputValue = e.target.value;
+        API.fetchApi(inputValue).then(console.log)
+   
+}
+
+
+
+
+function renderCard(obj) {
+    const card = imgCardTpl(obj);
+    console.log(card)
+    return refs.gallery.innerHTML = card;
+
+}
---
+++ src/js/fetchApi.js
@@ -0,0 +1,17 @@
+export default { fetchApi };
+
+const axios = require('axios');
+
+
+// fetch(URL).then(data => data.json()).then(console.log).catch(error => console.log("Error", error))
+
+async function fetchApi(name) {
+
+    const API_KEY = '22579303-973b9b71134c76d3c38c0933d';
+    const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    return await axios.get(URL).then(res => res.data)
+    // return await axios.get(URL).then(res => console.log(res.data))
+}
+
+
